b896fb70626115edeb5a3a002039a7375e171047
ZEGLAZI <zeglazi.oussama@gmail.com>
2024-05-17 23:31:37 +0200

    Data ingestion

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Text-summarizer-Project
+# End to end Text-summarizer-Project
+
+## Worflows
+
+1. Update config.yaml
+2. Update params.yaml
+3. Update entity
+4. Update the configuration manager is src config
+5. Update components
+6. Update the pipeline
+7. Update the main.py
+8. Update the app.py
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from TextSummarizer.logging import logger
+from src.TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
+from src.TextSummarizer.logging import logger
 
-logger.info("Welcom to our custom logging")
+STAGE_NAME = "Data Ingestion stage"
+
+try :
+    logger.info(f">>>>>>>> stage {STAGE_NAME} Started <<<<<<<<")
+    data_ingestion = DataIngestionTraningPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<")
+except Exception as e :
+    logger.exception(e)
+    raise e
+
+
+
